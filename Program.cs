7c616a9a105e8aee012a01018e17c39d1e444fae
JonnySiHI <Jonnysihi@me.com>
2017-01-13 12:56:38 +0000

    .
    .

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucillebot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lucillebot bot = new Lucillebot();
+
+
+        }
+    }
+}
---
+++ lucillebot.cs
@@ -0,0 +1,64 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+
+namespace Lucillebot
+{
+    class Lucillebot
+    {
+        DiscordClient discord;
+
+        public Lucillebot()
+        {
+            discord = new DiscordClient(x =>
+            {
+
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+
+            });
+
+            discord.UsingCommands(x =>
+            {
+
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("Hello").Do(async (e) =>
+
+            {
+                await e.Channel.SendMessage("Hey? what do you want?");
+
+            });
+
+            commands.CreateCommand("How are you?").Do(async (e) =>
+
+            {
+                await e.Channel.SendMessage("Good thanks, how are you? (not that i care at all)");
+
+
+            });
+
+
+            discord.ExecuteAndWait(async () =>
+
+           {
+
+               await discord.Connect("", TokenType.Bot);
+
+
+            });
+        }
+            
+
+    private void Log(object sender, LogMessageEventArgs e)
+    {
+
+        Console.WriteLine(e.Message);
+        }
+    }
+}
